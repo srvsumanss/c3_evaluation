d1b0835d3a5c6972bd3c76672fd1517730b3a290
srvsumanss <srvsumanss@gmail.com>
2022-03-21 16:43:33 +0530

    update

---
+++ Desktop/c3/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Navbar} from './Components/Navbar/Navbar'
+import {AllRoutes} from './Routes/Routes'
+
+function App() {
+  return (
+    <div className="App">
+     
+     <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/c3/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className="notFound">
+      <img src="https://www.answersreviews.com/wp-content/uploads/2020/06/Fix-Error404-on-WordPress-780x400.jpg"></img>
+    </div>
+  );
+};
