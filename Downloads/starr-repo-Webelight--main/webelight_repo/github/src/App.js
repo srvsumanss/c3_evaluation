5bf997750c1c9f571b54d1147b343a91ba5c2d89
Saurabh Suman <96100046+srvsumanss@users.noreply.github.com>
2022-09-07 21:37:09 +0530

    update

---
+++ Downloads/starr-repo-Webelight--main/webelight_repo/github/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import StarredRepo from './components/StarredRepo';
+
+function App() {
+  return (
+    <div className="App">
+      <StarredRepo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Downloads/starr-repo-Webelight--main/webelight_repo/github/src/components/ModalData.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import GitHubCalendar from 'react-github-calendar';
+import { useEffect, useState } from 'react';
+import ReactTooltip from 'react-tooltip';
+
+
+const ModalData = () => {
+    const [username, setUsername] = useState('grubersjoe');
+
+    let NameofUser = JSON.parse(localStorage.getItem('NameOfUser'))
+    console.log('NameofUser:', NameofUser)
+
+
+  return (
+    <>
+        <div>
+            <h4>User Activity Graph</h4>
+            <img src={`https://activity-graph.herokuapp.com/graph?username=${NameofUser}&bg_color=0D1117&color=5BCDEC&line=5BCDEC&point=FFFFFF&hide_border=true`} alt="" width='95%'/>
+
+            <GitHubCalendar username="srvsumanss" />
+        </div>
+    </>
+  )
+}
+
+export default ModalData
