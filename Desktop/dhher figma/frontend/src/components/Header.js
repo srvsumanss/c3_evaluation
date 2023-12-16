323bb76f258003dfb088827ed3018a85a964a7d7
Saurabh Suman <96100046+srvsumanss@users.noreply.github.com>
2023-12-17 02:51:52 +0530

    unted landing page

--- Desktop/dhher figma/frontend/src/components/Header.js
+++ Desktop/dhher figma/frontend/src/components/Header.js
@@ -9,14 +9,14 @@ function Header() {
    <nav className="fixed top-0 w-full h-auto flex items-center font-semibold justify-between flex-wrap p-4 bg-black z-10">
      <div className="flex items-center flex-shrink-0 text-white sm:mr-0 lg:mr-72">
        <img src={dheerLogo} className="w-100 h-10 rounded-xl mr-2" alt="Logo" />
-       <NavLink to="/" className="block mt-0 lg:inline-block lg:mt-0 text-white-200 ml-10">
+       <NavLink to="/" className="block mt-0 lg:inline-block lg:mt-0 text-white-200 ml-6">
            DHEER MARKETING (I) PVT LTD
          </NavLink>
      </div>
      <div className="block lg:hidden">
        <button
          onClick={() => setIsOpen(!isOpen)}
-         className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+         className="flex items-center px-3 py-2 rounded text-white hover:text-black-400"
        >
          <svg
            className={`fill-current h-3 w-3 ${isOpen ? "hidden" : "block"}`}
